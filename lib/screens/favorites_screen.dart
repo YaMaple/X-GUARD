f65f114962b86cf003cbe613d18361c7d97ae421
hjo <huang_jinou@163.com>
2021-10-07 20:26:07 +0800

    添加 TextField

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -3,34 +3,63 @@ import 'package:flutter/material.dart';
 import '../models/meal.dart';
 import '../widgets/meal_item.dart';
 
-class FavoritesScreen extends StatelessWidget {
+class FavoritesScreen extends StatefulWidget {
   final List<Meal> favoriteMeals;
 
   FavoritesScreen(this.favoriteMeals);
 
   @override
+  FavoritesScreenState createState() => FavoritesScreenState();
+}
+
+class FavoritesScreenState extends State<FavoritesScreen> {
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  void submitData() {
+    print("submit data");
+    print(titleController.text);
+    print(amountController.text);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    if (favoriteMeals.isEmpty) {
+    if (widget.favoriteMeals.isEmpty) {
       return Scaffold(
-        appBar: AppBar(
-          title: Text('Favorites'),
-        ),
-        body: Center(
-            child: Text('You have no favorites yet - start adding some!')),
-      );
+          appBar: AppBar(
+            title: Text('Favorites'),
+          ),
+          body: Container(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(labelText: 'Title'),
+                  controller: titleController,
+                  onSubmitted: (_) => submitData(),
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Amount'),
+                  controller: amountController,
+                  onSubmitted: (_) => submitData(),
+                ),
+              ],
+            ),
+          ));
     } else {
       return ListView.builder(
         itemBuilder: (ctx, index) {
           return MealItem(
-            id: favoriteMeals[index].id,
-            title: favoriteMeals[index].title,
-            imageUrl: favoriteMeals[index].imageUrl,
-            duration: favoriteMeals[index].duration,
-            affordability: favoriteMeals[index].affordability,
-            complexity: favoriteMeals[index].complexity,
+            id: widget.favoriteMeals[index].id,
+            title: widget.favoriteMeals[index].title,
+            imageUrl: widget.favoriteMeals[index].imageUrl,
+            duration: widget.favoriteMeals[index].duration,
+            affordability: widget.favoriteMeals[index].affordability,
+            complexity: widget.favoriteMeals[index].complexity,
           );
         },
-        itemCount: favoriteMeals.length,
+        itemCount: widget.favoriteMeals.length,
       );
     }
   }

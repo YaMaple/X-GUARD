3f31587df9343475b631de949857c02406b069fd
hjo <huang_jinou@163.com>
2021-11-09 19:11:45 +0800

    完成报表界面的手机展示

--- lib/screens/constants.dart
+++ lib/screens/constants.dart
@@ -1,7 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 const primaryColor = Color(0xFF2697FF);
 const secondaryColor = Color(0xFF2A2D3E);
 const bgColor = Color(0xFF212332);
 
 const defaultPadding = 16.0;
+
+const kGrey1 = Color(0xFF9F9F9F);
+const kGrey2 = Color(0xFF6D6D6D);
+const kGrey3 = Color(0xFFEAEAEA);
+const kBlack = Color(0xFF1C1C1C);
+
+var kNonActiveTabStyle = GoogleFonts.roboto(
+  textStyle: TextStyle(fontSize: 14.0, color: kGrey1),
+);
+
+var kActiveTabStyle = GoogleFonts.roboto(
+  textStyle: TextStyle(
+    fontSize: 16.0,
+    color: kBlack,
+    fontWeight: FontWeight.bold,
+  ),
+);
+
+var kCategoryTitle = GoogleFonts.roboto(
+  textStyle: TextStyle(
+    fontSize: 14.0,
+    color: kGrey2,
+    fontWeight: FontWeight.bold,
+  ),
+);
+
+var kDetailContent = GoogleFonts.roboto(
+  textStyle: TextStyle(
+    fontSize: 14.0,
+    color: kGrey2,
+  ),
+);
+
+var kTitleCard = GoogleFonts.roboto(
+  textStyle: TextStyle(
+    fontSize: 18.0,
+    color: kBlack,
+    fontWeight: FontWeight.bold,
+  ),
+);
+
+var descriptionStyle = GoogleFonts.roboto(
+    textStyle: TextStyle(
+  fontSize: 15.0,
+  height: 2.0,
+));
--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'constants.dart';
 
 import '../dummy_data.dart';
 
@@ -43,59 +44,37 @@ class MealDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('${selectedMeal.title}'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 18.0),
+        child: ListView(
           children: <Widget>[
             Container(
+              padding: EdgeInsets.symmetric(horizontal: 3.0, vertical: 15),
               height: 300,
-              width: double.infinity,
+              // decoration: BoxDecoration(
+              //   border: Border.all(color: kGrey3, width: 1.0),
+              //   borderRadius: BorderRadius.circular(25.0),
+              // ),
               child: Image(
                 image: AssetImage(selectedMeal.imageUrl),
                 fit: BoxFit.cover,
               ),
             ),
-            buildSectionTitle(context, 'Ingredients'),
-            buildContainer(
-              ListView.builder(
-                itemBuilder: (ctx, index) => Card(
-                  color: Theme.of(context).accentColor,
-                  child: Padding(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 5,
-                        horizontal: 10,
-                      ),
-                      child: Text(selectedMeal.ingredients[index])),
-                ),
-                itemCount: selectedMeal.ingredients.length,
-              ),
+            SizedBox(
+              height: 12.0,
             ),
-            buildSectionTitle(context, 'Steps'),
-            buildContainer(
-              ListView.builder(
-                itemBuilder: (ctx, index) => Column(
-                  children: [
-                    ListTile(
-                      leading: CircleAvatar(
-                        child: Text('# ${(index + 1)}'),
-                      ),
-                      title: Text(
-                        selectedMeal.steps[index],
-                      ),
-                    ),
-                    Divider()
-                  ],
-                ),
-                itemCount: selectedMeal.steps.length,
-              ),
+            Text("My Title,", style: kTitleCard.copyWith(fontSize: 28.0)),
+            SizedBox(
+              height: 12.0,
             ),
+            Text(
+              '''The history of modern finance is littered with ideas that worked well enough at small scale—railway bonds, Japanese skyscrapers, sliced-and-diced mortgage securities—but morphed into monstrosities once too many punters piled in. When it comes to sheer size, no mania can compare with that for passive investing. Funds that track the entire market by buying shares in every company in America’s s&p 500, say, rather than guessing which will perform better than average, have attained giant scale. Fully 40% of the total net assets managed by funds in America are in passive vehicles, reckons the Investment Company Institute, an industry group. The phenomenon warrants scrutiny.
+
+Index funds have grown because of the validity of the core insight underpinning them: conventional investment funds are, by and large, a terrible proposition. The vast majority fail to beat the market over the years. Hefty management fees paid by investors in such ventures, often around 1-2% a year (and more for snazzy hedge funds), add up to giant bonuses for stockpickers. Index funds, by contrast, charge nearly nothing (0.04% for a large equity fund) and do a good job of hugging their chosen benchmark. Given time, they almost inevitably leave active managers in the dust.''',
+              style: descriptionStyle,
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(
-          isFavorite(mealId) ? Icons.star : Icons.star_border,
-        ),
-        onPressed: () => toggleFavorite(mealId),
       ),
     );
   }

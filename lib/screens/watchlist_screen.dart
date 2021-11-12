f9f44e1820809bcf006b2b73b54a23719a4b0287
hjo <huang_jinou@163.com>
2021-11-12 15:54:59 +0800

    初步完成关注列表

--- lib/screens/watchlist_screen.dart
+++ lib/screens/watchlist_screen.dart
@@ -1,7 +1,120 @@
 import 'package:flutter/material.dart';
 
+class CardDetail {
+  String title;
+  String subtitle_1;
+  String subtitle_2;
+
+  CardDetail(
+      {required this.title,
+      required this.subtitle_1,
+      required this.subtitle_2});
+}
+
+class CardListTile extends StatelessWidget {
+  final String title;
+  final String subtitle_1;
+  final String subtitle_2;
+
+  CardListTile(
+      {required this.title,
+      required this.subtitle_1,
+      required this.subtitle_2});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(5),
+        ),
+      ),
+      color: Colors.white,
+      elevation: 8.0,
+      margin: new EdgeInsets.symmetric(
+        horizontal: 10.0,
+        vertical: 10.0,
+      ),
+      child: ListTile(
+        contentPadding: EdgeInsets.symmetric(
+          horizontal: 20.0,
+          vertical: 5.0,
+        ),
+        leading: Container(
+          padding: EdgeInsets.only(right: 12.0),
+          decoration: BoxDecoration(
+            border: Border(
+              right: BorderSide(
+                width: 1.0,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          child: Text(
+            'High Risk',
+            style: TextStyle(
+                fontStyle: FontStyle.normal,
+                color: Colors.red,
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+        title: Text(
+          title,
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Row(
+          children: [
+            Icon(
+              Icons.star,
+              color: Colors.yellow[900],
+            ),
+            Icon(
+              Icons.star,
+              color: Colors.yellow[900],
+            ),
+            Icon(
+              Icons.star,
+              color: Colors.yellow[900],
+            ),
+            Icon(
+              Icons.star,
+              color: Colors.yellow[900],
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text(
+              subtitle_1,
+              style: TextStyle(color: Colors.black),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text(
+              subtitle_2,
+              style: TextStyle(color: Colors.black),
+            )
+          ],
+        ),
+        trailing: Icon(
+          Icons.keyboard_arrow_right,
+          color: Colors.grey,
+          size: 30.0,
+        ),
+      ),
+    );
+  }
+}
+
 class WatchListScreen extends StatelessWidget {
   static const routeName = '/filters';
+
+  final List<CardDetail> cards = [
+    CardDetail(title: 'Meta', subtitle_1: '\$200,000', subtitle_2: '2017'),
+    CardDetail(title: 'LinkedIn', subtitle_1: '\$30,000', subtitle_2: '2018'),
+    CardDetail(title: 'Amazon', subtitle_1: '\$15,000', subtitle_2: '2019'),
+    CardDetail(title: 'Google', subtitle_1: '\$80,0000', subtitle_2: '2021'),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -10,112 +123,14 @@ class WatchListScreen extends StatelessWidget {
         title: Text('Watch List'),
         backgroundColor: Color.fromARGB(0xff, 0x14, 0x27, 0x4e),
       ),
-      body: const MyStatelessWidget(),
-    );
-  }
-}
-
-class CustomListItem extends StatelessWidget {
-  const CustomListItem({
-    Key? key,
-    required this.thumbnail,
-    required this.title,
-    required this.user,
-    required this.viewCount,
-  }) : super(key: key);
-
-  final Widget thumbnail;
-  final String title;
-  final String user;
-  final int viewCount;
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15),
-          ),
-          elevation: 4,
-          margin: EdgeInsets.all(10),
-          child: Column(children: <Widget>[
-            Padding(
-              padding: EdgeInsets.fromLTRB(10, 5, 0, 0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "ffff",
-                  style: TextStyle(fontSize: 26),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Text('Loan balance'),
-                      SizedBox(
-                        width: 6,
-                      ),
-                      Text('min'),
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Text('Warning level'),
-                      SizedBox(
-                        width: 6,
-                      ),
-                      Text('222'),
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Text('Repayment date'),
-                      SizedBox(
-                        width: 6,
-                      ),
-                      Text('33'),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ])),
-    );
-  }
-}
-
-/// This is the stateless widget that the main application instantiates.
-class MyStatelessWidget extends StatelessWidget {
-  const MyStatelessWidget({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      padding: const EdgeInsets.all(8.0),
-      // itemExtent: 106.0,
-      children: <CustomListItem>[
-        CustomListItem(
-          user: 'Flutter',
-          viewCount: 999000,
-          thumbnail: Container(
-            decoration: const BoxDecoration(color: Colors.blue),
-          ),
-          title: 'The Flutter YouTube Channel',
+      body: ListView.builder(
+        itemCount: cards.length,
+        itemBuilder: (context, index) => CardListTile(
+          title: cards[index].title,
+          subtitle_1: cards[index].subtitle_1,
+          subtitle_2: cards[index].subtitle_2,
         ),
-        CustomListItem(
-          user: 'Dash',
-          viewCount: 884000,
-          thumbnail: Container(
-            decoration: const BoxDecoration(color: Colors.yellow),
-          ),
-          title: 'Announcing Flutter 1.0',
-        ),
-      ],
+      ),
     );
   }
 }
